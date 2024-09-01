161e414065d9cdf9dcf700b3c024f9f340070aa7
Erfan Ansari <dev.erfanansari@gmail.com>
2024-09-01 23:24:14 +0330

    chore: bottom padding sucks and this did not fix it

--- .wezterm.lua
+++ .wezterm.lua
@@ -28,4 +28,8 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
+config.window_padding = {
+	bottom = 0,
+}
+
 return config
