ce6b96ca55c52d769d0b9ccb95e2ebd28fdada5a
Erfan Ansari <dev.erfanansari@gmail.com>
2025-09-29 15:04:33 +0330

    chore: update format-mr-title

--- misc/raycast-scripts/format-mr-title.sh
+++ misc/raycast-scripts/format-mr-title.sh
@@ -5,20 +5,29 @@
 # @raycast.title Format MR Title
 # @raycast.mode silent
 # @raycast.icon 📝
-# @raycast.packageName Formatter
+# @raycast.packageName GitLab Tools
 
 # Get clipboard content
 input=$(pbpaste)
 
-# Transform it using perl
+# Transform it using perl - handles Fix, Feat, Feature, Chore, etc.
 output=$(echo "$input" | perl -pe '
     s/^Draft: ?/[DRAFT]/i;
     s/Fix\//[FIX]/i;
+    s/Feat(ure)?\//[FEAT]/i;
+    s/Chore\//[CHORE]/i;
+    s/Refactor\//[REFACTOR]/i;
+    s/Docs\//[DOCS]/i;
+    s/Test\//[TEST]/i;
+    s/Style\//[STYLE]/i;
+    s/Perf\//[PERF]/i;
+    s/Build\//[BUILD]/i;
+    s/CI\//[CI]/i;
     s/crm[- ]?(\d+)/[CRM-$1]:/i;
     s/(: )([a-z])/$1\u$2/;
 ')
 
-# Put it back in clipboard
-echo -n "$output" | pbcopy
+# Type it at cursor position using AppleScript
+osascript -e "tell application \"System Events\" to keystroke \"$output\""
 
-echo "Formatted: $output"
+echo "✅ Typed: $output"
