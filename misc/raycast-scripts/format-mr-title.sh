984434a8e43d209e2e5f4115d6d35f3f2e676b96
Erfan Ansari <dev.erfanansari@gmail.com>
2026-01-16 14:57:13 +0330

    feat: update format-mr-title to paste on selected text

--- misc/raycast-scripts/format-mr-title.sh
+++ misc/raycast-scripts/format-mr-title.sh
@@ -6,13 +6,14 @@
 # @raycast.icon 📝
 # @raycast.packageName GitLab Tools
 
-# Get clipboard content
+# Copy and immediately read (no wait)
+osascript -e 'tell application "System Events" to keystroke "c" using command down'
 input=$(pbpaste)
 
-# Transform it using perl
+# Transform
 output=$(echo "$input" | perl -pe '
-    s/^Draft: ?//i;                                      # Remove existing Draft: prefix
-    s/^(\w+)\(crm[- ]?(\d+)\):/[\U$1\E][CRM-$2]:/i;     # Handle type(crm-123): format
+    s/^Draft: ?//i;
+    s/^(\w+)\(crm[- ]?(\d+)\):/[\U$1\E][CRM-$2]:/i;
     s/^Fix\//[FIX]/i;
     s/^Hotfix\//[HOTFIX]/i;
     s/^Feat(ure)?\//[FEAT]/i;
@@ -24,11 +25,10 @@ output=$(echo "$input" | perl -pe '
     s/^Perf\//[PERF]/i;
     s/^Build\//[BUILD]/i;
     s/^CI\//[CI]/i;
-    s/(?<!\[)crm[- ]?(\d+)/[CRM-$1]:/i;                 # Handle standalone crm-123 (not already in brackets)
-    s/^(?!\[DRAFT\])/[DRAFT]/;                          # Add [DRAFT] at beginning if not present
-    s/(: )([a-z])/$1\u$2/;                              # Capitalize first letter after ": "
+    s/(?<!\[)crm[- ]?(\d+)/[CRM-$1]:/i;
+    s/^(?!\[DRAFT\])/[DRAFT]/;
+    s/(: )([a-z])/$1\u$2/;
 ')
 
-# Type it at cursor position using AppleScript
+# Type replacement
 osascript -e "tell application \"System Events\" to keystroke \"$output\""
-echo "✅ Typed: $output"
